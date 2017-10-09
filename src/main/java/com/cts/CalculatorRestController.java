1eb4d2a55e538112e9b72a1a394722f29f3c55c0
skrish018c <Sunil_Krishna@cable.comcast.com>
2017-10-09 17:23:58 +0530

    Commented Code Cleanup

--- src/main/java/com/cts/CalculatorRestController.java
+++ src/main/java/com/cts/CalculatorRestController.java
@@ -40,27 +40,5 @@ public class CalculatorRestController {
     public long pow(@RequestParam long... operands) {
         return mathOperator.pow(operands);
     }
-//
-//    @GetMapping("/reverse")
-//    public String reverse(@RequestParam String str) {
-//        if (str == null || str.isEmpty()) {
-//            return str;
-//        }
-//        char[] characters = str.toCharArray();
-//        int i = 0;
-//        int j = characters.length - 1;
-//        while (i < j) {
-//            swap(characters, i, j);
-//            i++;
-//            j--;
-//        }
-//        return new String(characters);
-//    }
-//
-//    private void swap(char[] str, int i, int j) {
-//        char temp = str[i];
-//        str[i] = str[j];
-//        str[j] = temp;
-//    }
 
 }
